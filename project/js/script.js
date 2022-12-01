d83806794ee57b772d0a2aab51b2b9e828e4c53b
Yuna <example@gmail.com>
2022-12-01 13:35:08 +0300

    first task on movieDB

--- project/js/script.js
+++ project/js/script.js
@@ -12,15 +12,52 @@
 
 5) Добавить нумерацию выведенных фильмов */
 
-'use strict';
+"use strict";
+
+const promo = document.querySelectorAll(".promo__adv img");
+
+promo.forEach((item) => {
+  item.remove();
+});
+const genre = document.querySelector(".promo__genre");
+genre.textContent = "Драма";
+
+const bg = document.querySelector(".promo__bg");
+bg.style.backgroundImage = "url('img/bg.jpg')";
+
+console.log(bg);
 
 const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
+  movies: [
+    "Логан",
+    "Лига справедливости",
+    "Ла-ла лэнд",
+    "Одержимость",
+    "Скотт Пилигрим против...",
+  ],
 };
 
+const filmUl = document.querySelector(".promo__interactive-list");
+
+movieDB.movies.sort();
+// movieDB.movies.forEach((film, index) => {
+//   let title = document.createElement("li");
+//   title.classList.add("promo__interactive-item");
+//   title.innerText = `${index + 1}.  ${film}`;
+//   filmUl.append(title);
+
+//   const del = document.createElement("div");
+//   del.classList.add("delete");
+//   title.append(del);
+
+// });
+
+filmUl.innerHTML = "";
+
+movieDB.movies.forEach((film, index) => {
+  filmUl.innerHTML += `
+    <li class="promo__interactive-item">${index + 1}.   ${film}
+    <div class="delete"></div>
+</li>
+    `;
+});
